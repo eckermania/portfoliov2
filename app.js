613daf890bca4f11a47cee991b871a1361955cd6
Erin Eckerman <erineckerman@gmail.com>
2018-11-11 12:36:46 -0800

    moved jquery into public

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 'use strict';
 
-// Hide/Show Menu in Mobile View
+const express = require('express');
+const app = express();
 
-$(document).ready(function() {
-    $('header i').on('click', function(){
-        $('.menu').toggleClass('hidden');
-      });
-    
-    $('header i').on('click', function(){
-        $('#mobile-nav').toggleClass('hidden');
-        });
-  });
+const PORT = process.env.PORT || 3000;
 
+app.use(express.static('./public'));
 
+app.get('/', (request, response) => {
+  response.sendFile('index.html', {root: './public'});
+});
 
+app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
+
+app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
---
+++ public/js/server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Hide/Show Menu in Mobile View
+
+$(document).ready(function() {
+  $('header i').on('click', function(){
+    $('.menu').toggleClass('hidden');
+  });
+
+  $('header i').on('click', function(){
+    $('#mobile-nav').toggleClass('hidden');
+  });
+});
