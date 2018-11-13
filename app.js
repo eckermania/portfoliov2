c7068e67f0aac8aed3e60639df38d8fc0bd13a47
Erin Eckerman <erineckerman@gmail.com>
2018-11-13 12:33:13 -0800

    testing example page

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.get('/', (request, response) => {
   response.sendFile('index.html', {root: './public'});
 });
 
+app.get('/', (request, response) => {
+  response.sendFile('example1.html', {root: './public'});
+});
+
 app.use('*', (request, response) => response.send('Sorry, that route does not exist.'))
 
 app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
